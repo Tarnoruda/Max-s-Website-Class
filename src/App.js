8bc31d1d77a662e314f9931783190cce6c735a30
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2020-10-23 17:43:44 +0200

    transformed function component to class component, commented out JSX part of the code to test functionality that happens behind the scene of JSX with one nested element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>I am an React App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <h1>I am an React App</h1>
+    //   </div>
+    // );
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "Is this even working?")
+    );
+  }
 }
 
 export default App;
