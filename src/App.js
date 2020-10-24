48a1aa24b44f3818be1b3cd1701ddd3c8412ac70
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2020-10-24 17:41:37 +0200

    Added first React component - Person

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>I am an React App</h1>
-    //   </div>
-    // );
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Is this even working?")
+    return (
+      <div className="App">
+        <h1>I am an React App</h1>
+        <p>This is really working</p>
+        <Person />
+      </div>
     );
   }
 }
