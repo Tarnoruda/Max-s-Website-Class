a5f62f6cb90f5d73ed70f61dcce2e24b0f5967df
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2020-11-08 10:30:33 +0100

    removed switchNameHandler and replaced it with deletePersonHandler transformed list of persons from hard-coded to dynamically generated by map method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 import UserOutput1 from "./Assignments/Assignment1/UserOutput1";
+import person from "./Person/Person";
 
 class App extends Component {
   state = {
@@ -12,16 +13,6 @@ class App extends Component {
     ],
     assignment1: ["PlayerOne", "PlayerTwo"],
     showPersons: false,
-  };
-
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 29 },
-        { name: "Johnathan", age: 26 },
-        { name: "Xin-Xiao", age: 231 },
-      ],
-    });
   };
 
   nameChangerHandler = (event) => {
@@ -38,6 +29,12 @@ class App extends Component {
     this.setState({
       assignment1: [event.target.value, "PlayerTwo"],
     });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -59,22 +56,15 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Max!")}
-            changed={this.nameChangerHandler}
-          >
-            Hobbies: Books
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+              />
+            );
+          })}
         </div>
       );
     }
