1a58c11cccf5c94961c512554f11fcd92bdde986
Tarnoruda <m.tarnoruda@gmail.com>
2020-11-28 20:41:07 +0100

    dynamically modification of styles through modification of single attribute on toggle and addition of css classes when removing persons elements

--- src/App.js
+++ src/App.js
@@ -80,11 +80,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
+      color: "white"
     };
 
     let persons = null;
@@ -106,6 +107,7 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
     }
 
     let assignmentOnePersons = (
@@ -122,10 +124,18 @@ class App extends Component {
       </div>
     );
 
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <=1) {
+      classes.push('bold');
+    }
+
     return (
       <div className="App">
         <h1>I am an React App</h1>
-        <p>This is really working</p>
+        <p className={classes.join(" ")}>This is really working</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
